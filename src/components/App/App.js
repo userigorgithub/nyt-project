4163fc108f0ab61e8f175c70be65eb3fc757bd99
Igor Decess <i.decess@comcast.net>
2022-07-26 19:04:42 -0600

    Clean up App file and add other folders and files according to application's architecture

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import React, { useState } from "react";
+
+
+
+
+const App = () => {
+
+
+
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App;
