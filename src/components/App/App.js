cd362328dee1f4213026eae9f476e0be9028401e
Igor Decess <i.decess@comcast.net>
2022-07-26 22:05:35 -0600

    Display Articles word in Articles file through passing prop from App file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import React, { useState } from "react";
+import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
+import Articles from '../Articles/Articles';
 import Footer from '../Footer/Footer';
 
 
-const App = () => {
 
+const App = () => {
+  const [articles, setArticles] = useState([]);
 
 
 
   return (
     <div className="App">
       <Header/>
+      <Routes>
+      <Route path='/'
+        element={<Articles articles={articles}/>}
+      />
+
+      </Routes>
       <Footer/>
     </div>
   )
---
+++ src/components/Articles/Articles.js
@@ -0,0 +1,13 @@
+import './Articles.css';
+import React from 'react';
+
+const Articles = ({ articles }) => {
+
+  return (
+    <div className='Articles'>
+      <p>Articles</p>
+    </div>
+  )
+}
+
+export default Articles;
